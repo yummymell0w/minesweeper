c07c04782fa5a1067203964e028a869eadeb666d
Margo Art <margoart.work@gmail.com>
2017-10-21 18:35:41 -0500

    minesweeper 1 renamed to minesweeper_hardcoded completed minesweeper 2 exercise

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,15 @@
-const blankLine = '  |   |  ';
-console.log("This is what an empty board would look like:");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-console.log("This is what a board with a guess and a bomb on it would look like:");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const printBoard = board => {
+    console.log('Current Board:');
+    console.log(board[0].join('|'));
+    console.log(board[1].join('|'));
+    console.log(board[2].join('|'));
+};
+let board = [
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' ']
+];
+printBoard(board);
+board[0][1] = 1;
+board[2][2] = 'B';
+printBoard(board);
