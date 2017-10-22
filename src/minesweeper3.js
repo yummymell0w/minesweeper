2649caf7a6a0f7d1e3f70d744ec84d523e2c8fe7
Margo Art <margoart.work@gmail.com>
2017-10-22 16:18:27 -0500

    progress save

--- src/minesweeper3.js
+++ src/minesweeper3.js
@@ -21,16 +21,24 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     }
     //Randomly Place Bombs on the Bomb Board
     let numberOfBombsPlaced = 0;
+    //An important note: The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
     while (numberOfBombsPlaced < numberOfBombs) {
         let randomRowIndex = Math.floor(Math.random() * numberOfRows);
         let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-        board[numberOfRows][numberOfColumns] = 'B';
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced ++;
     }
-
-
-
-
     return board;
 };
-
-
+//An Updated printBoard() Function
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+//Create Both Boards
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+//Print Both Boards
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
